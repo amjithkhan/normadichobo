ce10a63b7d7601e79826dd2d36c39f69cafbb51a
AMJITHKHAN <amjithkhan196@hotmail.com>
2022-09-13 22:22:14 -0700

    product was created by admin page, static backup files was settedc named as asset,  template cutting for dynamic product loading, media files created for product image saving .

--- normadichobo/normadichobo/settings.py
+++ normadichobo/normadichobo/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from msilib.schema import Media
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
+    'product',
+
 ]
 
 MIDDLEWARE = [
@@ -122,8 +126,13 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,"static")
 ]
-
+STATIC_ROOT=os.path.join(BASE_DIR,"asset")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#mediafiles
+Media_url="/media/"
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
